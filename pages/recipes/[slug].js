3d7cea8ad23dc6fec9712d6d574944a67fba768d
Marco Budiongan <marcobdngn@gmail.com>
2021-09-16 20:13:16 +0800

    generating paths

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,3 +1,41 @@
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+import contentful from "../../contentful";
+
+const client = contentful.getClient();
+
+export async function getStaticPaths() {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const paths = items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  const [recipe] = items;
+
+  return {
+    props: { recipe },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  const {
+    fields: { title },
+  } = recipe;
+
+  return <div>{title}</div>;
 }
